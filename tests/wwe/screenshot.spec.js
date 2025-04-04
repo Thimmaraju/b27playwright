23715e488171ef209f98b1cc9b80869cdde855f3
G Thimmaraju <thimmaraju418@gmail.com>
2025-04-04 11:18:52 +0530

    wwe5

--- tests/wwe/screenshot.spec.js
+++ tests/wwe/screenshot.spec.js
@@ -4,9 +4,9 @@ test('page screenshot', async ({ page }) => {
     await page.goto('https://www.flipkart.com/')
 
     await page.waitForTimeout(3000)
-    await page.screenshot({ path: 'tests/screenshots/HomePage.png' })
+    //await page.screenshot({ path: 'tests/screenshots/HomePage.png' })
 
-    //await page.screenshot({ path:'tests/screenshots/'+Date.now()+'HomePage.png'})
+    await page.screenshot({ path:'tests/screenshots/'+Date.now()+'HomePage.png'})
 });
 
 test('Full page screenshot', async ({ page }) => {
@@ -19,4 +19,13 @@ test('Element screenshot', async ({ page }) => {
     await page.goto('https://www.flipkart.com/')
     await page.waitForTimeout(3000)
     await page.locator('div[class="_3bzdSa"]').screenshot({ path: 'tests/screenshots/' + Date.now() + '.png' })
+});
+
+
+test('Element screenshot Example 2 ', async ({ page }) => {
+
+    console.log("Im taking screenshot")
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.waitForTimeout(3000)
+    await page.locator('//img[@alt="company-branding"]/..').screenshot({ path: 'tests/screenshots/' + Date.now() + 'element.png' })
 });
